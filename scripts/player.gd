05d45352efee728ac092421a352f31d7c6cb3e5c
Swedgar13 <edgar.tegelaar@gmail.com>
2026-03-16 18:04:51 +0100

    added cayote time

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite_group: Node2D = $"Sprite Group"
 @onready var original_scale_x: float = sprite_group.scale.x
+@onready var timer: Timer = $Timer
+
+@export var cayote_time: float = 1
 
 const MAX_SPEED = 300
 const ACCELERATION = 400.0
@@ -10,18 +13,26 @@ const DECELERATION = 300.0
 const JUMP_VELOCITY = 600.0
 const ANIMATION_SPEED_MULTIPLIER = 1.2
 
+var can_jump : bool = true
+
+func _ready() -> void:
+	timer.wait_time = cayote_time
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		timer.start()
 		if velocity.y < 0:
 			velocity += get_gravity() * delta * 3
 		else:
 			velocity += get_gravity() * delta * 0.8
-
+	else: 
+		can_jump = true
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("move_jump(space)") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump(space)") and can_jump == true:
 		velocity.y = -JUMP_VELOCITY
+		can_jump = false
 
 	var direction := Input.get_axis("move_left(a)", "move_right(d)")
 	if direction:
@@ -40,3 +51,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		animation_player.play("idle")
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	can_jump = false
